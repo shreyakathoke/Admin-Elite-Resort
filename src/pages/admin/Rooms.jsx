c1b0e26c01f7c6812ff1600191d9b9a94f6aafd5
shreyakathoke <kathokeshreya03@gmail.com>
2026-02-25 10:28:42 +0530

    eigth

--- src/pages/admin/Rooms.jsx
+++ src/pages/admin/Rooms.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/rooms.css";
 
-import { deleteAdminRoom, getAdminRooms } from "../../api/adminRoomsApi";
+import { deleteAdminRoom, getAdminRooms } from "../../api/roomsApi";
 
 export default function Rooms() {
   const navigate = useNavigate();
